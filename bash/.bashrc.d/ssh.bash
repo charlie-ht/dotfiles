319ebd4c4e9c72eeef9ec002c8cfa2d9f82cc26d
Charles <cht886521@pm.me>
2020-07-28 07:23:14 +0100

    Do not perform SSH stuff in .bashrc
    SSH / keys are a confusing item in GNU/Linux. GNOME manages them in
    its own way. Managing them from shells assumes everything is launched
    from the shell, which in my case it is not. You can also start agents
    using systemd, but I guess that messes with GNOME and the confusion
    circles round.
    
    For now, it seems to WFM in GNOME, so I'll Occam this shell file.

--- bash/.bashrc.d/ssh.bash
+++
@@ -1,6 +0,0 @@
- #https://unix.stackexchange.com/questions/90853/how-can-i-run-ssh-add-automatically-without-password-prompt
- if [ -z "$SSH_AUTH_SOCK" ] ; then
-     { eval `ssh-agent -s` ; ssh-add ; } &>/dev/null
- fi
-
- trap 'test -n "$SSH_AUTH_SOCK" && eval `/usr/bin/ssh-agent -k` &> /dev/null' 0
