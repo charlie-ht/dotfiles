30d9331bf84701766fe1a27436b1b2d686e986c1
Charles <cht886521@pm.me>
2020-10-23 00:44:26 +0100

    bash: add $HOME/src to CDPATH

--- bash/.bashrc.d/bash_options.bash
+++ bash/.bashrc.d/bash_options.bash
@@ -7,3 +7,5 @@ shopt -s checkwinsize
 # If set, the pattern "**" used in a pathname expansion context will
 # match all files and zero or more directories and subdirectories.
 shopt -s globstar
+
+export CDPATH=$CDPATH:$HOME/src/
