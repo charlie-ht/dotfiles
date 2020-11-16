b275490e2f9ad82040f7144323c951c76f815514
Charles <cht886521@pm.me>
2020-11-16 13:04:10 +0000

    Workaround stale sshfs mounts after suspend

--- misc/bin/mount-remotes.sh
+++ misc/bin/mount-remotes.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-sshfs router:/ $HOME/remotes/router
-sshfs -o cache_timeout=$((60 * 60)) router:/mnt/extstorage $HOME/remotes/router-extstorage
+# https://serverfault.com/questions/6709/sshfs-mount-that-survives-disconnect/639735
+KEEP_ALIVE_OPTS='reconnect,ServerAliveInterval=15,ServerAliveCountMax=3'
+sshfs -o $KEEP_ALIVE_OPTS router:/ $HOME/remotes/router
+# The cache timeout here is long because I only edit things from my
+# workstation, not from the router, so invalidation is not really an
+# issue.
+sshfs -o $KEEP_ALIVE_OPTS -o cache_timeout=$((60 * 60)) router:/mnt/extstorage $HOME/remotes/router-extstorage
 
