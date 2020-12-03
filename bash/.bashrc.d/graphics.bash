48b9b1351c621f3ebccc7fc2e75e9c221e1bd198
Charles <cht886521@pm.me>
2020-12-03 06:59:41 +0000

    Allow spaces in search terms

--- bash/.bashrc.d/graphics.bash
+++ bash/.bashrc.d/graphics.bash
@@ -1,5 +1,5 @@
 export CDPATH=$CDPATH:$HOME/igalia/graphics/
 
 function mesa_grep_ci() {
-    grep -rn $@ $HOME/src/mesa/.gitl* | grep -v :0
+    grep -rn "$@" $HOME/src/mesa/.gitl* | grep -v :0
 }
