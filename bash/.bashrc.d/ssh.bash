333057947060f38d85d650093cff5ff9b2ac0d1a
Charles <cht886521@pm.me>
2021-01-26 15:38:12 +0000

    ssh: Forward agent to selected hosts
    If an agent is already running on the host, be prepared for a
    a confusing moment

--- bash/.bashrc.d/ssh.bash
+++ bash/.bashrc.d/ssh.bash
@@ -1,1 +1,4 @@
-eval `keychain --eval --agents ssh id_rsa_charlie id_ed25519_charlie id_rsa_igalia`
+# Only start keychain on machines we have not SSH'd to.
+# For those that we have SSH'd, SSH agent-forwarding should be all
+# that is needed.
+[ "$SSH_CONNECTION" ] || eval `keychain --eval --agents ssh id_rsa_charlie id_ed25519_charlie id_rsa_igalia`
