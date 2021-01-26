9d8d98ad1ff5be991cf390d198a712821ebb826a
Charles <cht886521@pm.me>
2021-01-26 15:50:03 +0000

    ssh: Project against server-side agents
    These will clobber the forwarding, make it less silent!

--- bash/.bashrc.d/ssh.bash
+++ bash/.bashrc.d/ssh.bash
@@ -1,3 +1,11 @@
+if [ "$SSH_CONNECTION" ] && pgrep ssh-agent &> /dev/null 2>&1 ; then
+    echo "WARNING: You are in a SSH hurt locker!!!"
+    echo "WARNING: You are forwarding an SSH agent, yet their is an agent already running on this server"
+    echo "WARNING: That will only lead to doom"
+    echo "The agent on this host has PID $(pgrep ssh-agent)"
+    echo "Try making it stop starting up!"
+fi
+
 # Only start keychain on machines we have not SSH'd to.
 # For those that we have SSH'd, SSH agent-forwarding should be all
 # that is needed.
